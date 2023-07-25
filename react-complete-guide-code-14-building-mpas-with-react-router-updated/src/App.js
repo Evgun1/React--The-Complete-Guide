ff39dd68c30531f11b8fe2466536788ed5d9aa09
Evgun1 <eugenegavrick@gmail.com>
2023-07-25 11:39:42 +0300

    second

---
+++ react-complete-guide-code-14-building-mpas-with-react-router-updated/src/App.js
@@ -0,0 +1,39 @@
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  RouterProvider,
+  // Route,
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductsDetailPage from "./pages/ProductDetail";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductPage />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> }, //path: ''
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductsDetailPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
